0c55342774a2377899782261704ba60d68fae410
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2024-02-04 16:07:04 -0600

    Update search_all_jokes.php

--- search_all_jokes.php
+++ search_all_jokes.php
@@ -1,7 +1,7 @@
 <?php
 include "db_connect.php";
 
-$sql = "SELECT JokeID, Joke_question, Joke_answer, user_id, username, google_name FROM Jokes_table JOIN users ON jokes_table as user_id = users.id";
+$sql = "SELECT JokeID, Joke_question, Joke_answer, user_id, username, google_name FROM Jokes_table JOIN users ON jokes_table";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
@@ -9,7 +9,7 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<h3>" . $row['Joke_question'] . "</h3>";
 
-        echo "<div><p>" . $row["Joke_answer"] . " submitted by user #" . $row['userid'] . " whose name is " . $row['username'] . $row['google_name'] . "</p></div>";
+        echo "<div><p>" . $row["Joke_answer"] . " submitted by user #" . $row['user_id'] . " whose name is " . $row['username'] . $row['google_name'] . "</p></div>";
     }
 } else {
     echo "0 results";
