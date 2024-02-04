28c57b721d3c0334150cad3814861cb9a63bc724
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2024-02-04 15:45:19 -0600

    update

--- add_joke.php
+++ add_joke.php
@@ -16,7 +16,7 @@ $userid = $_SESSION['userid'];
 
 echo "<h2>Trying to add a new joke " . $new_joke_question . " and " . $new_joke_answer . "</h2>";
 
-$sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer, userid) VALUES (null, '$new_joke_question', '$new_joke_answer', '$userid')";
+$sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer, user_id) VALUES (null, '$new_joke_question', '$new_joke_answer', '$userid')";
 
 $result = $mysqli->query($sql) or die (mysqli_error($mysqli));
 
